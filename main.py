c787cc84574bc6a5c7a3ffc7c00f688edfcb5ec8
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:28:52 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Foydalanuvchi Tomonidan Baholar Royxati Tuple Korinishida Fu
+Topic: Foydalanuvchi tomonidan baholar ro‘yxati `tuple` ko‘rinishida funksiyaga uzatiladi.
+"""
+
+def main():
+    print("Welcome to Foydalanuvchi Tomonidan Baholar Royxati Tuple Korinishida Fu!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
